0f334fcb83acbdb8f75b11af81414e1ee26d81a6
devosamagasser <devosamagasser@gmail.com>
2025-03-17 03:47:50 +0200

    Announcements & Node response

--- database/migrations/2024_11_24_185948_create_announcements_table.php
+++ database/migrations/2024_11_24_185948_create_announcements_table.php
@@ -15,14 +15,13 @@ return new class extends Migration
         Schema::create('announcements', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users');
-            $table->foreignId('department_id')->nullable()->constrained('departments')->unllOnDelete();
-            $table->foreignId('semester_id')->nullable()->constrained('semesters')->unllOnDelete();
-            $table->foreignId('course_id')->nullable()->constrained('courses')->unllOnDelete();
+            $table->foreignId('department_id')->nullable()->constrained('departments')->nullOnDelete();
+            $table->foreignId('semester_id')->nullable()->constrained('semesters')->nullOnDelete();
+            $table->foreignId('course_id')->nullable()->constrained('courses')->nullOnDelete();
             $table->string('title')->nullable();
-            $table->text('body')->nullable();
-            $table->string('cover')->nullable();
-            $table->timestamp('time_to_post')->useCurrent();
-            $table->time('time')->default(DB::raw('CURRENT_TIME'));
+            $table->text('body');
+            $table->timestamp('time_to_post')->default(now()->toDateString());
+            $table->time('time')->nullble();
             $table->timestamps();
         });
     }
