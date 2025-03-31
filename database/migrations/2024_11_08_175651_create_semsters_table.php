687f68f0e205c1a37dc0dd3b8f7b311b1b2269ca
devosamagasser <devosamagasser@gmail.com>
2025-03-31 22:35:16 +0200

    quize

--- database/migrations/2024_11_08_175651_create_semsters_table.php
+++ database/migrations/2024_11_08_175651_create_semsters_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('semesters', function (Blueprint $table) {
             $table->id();
             $table->tinyInteger('levels');
-            $table->string('name');
             $table->timestamps();
         });
     }
