09b40c840c10a9c7c84717718167535b312a0042
devosamagasser <devosamagasser@gmail.com>
2025-04-22 15:30:57 +0200

    teachers departments and semesters v1.0.1

--- routes/teachers.php
+++ routes/teachers.php
@@ -9,8 +9,8 @@ use App\Modules\Teachers\TeachersController;
 
 Route::group(['middleware'=>['auth','role:Teacher']],function (){
     Route::get('/courses',[TeachersController::class,'myCourses']);
-    Route::get('/departments',[TeachersController::class,'getDepartments']);
-    Route::get('/semesters',[TeachersController::class,'getSemesters']);
+    Route::get('/departments',[TeachersController::class,'myDepartments']);
+    Route::get('/semesters',[TeachersController::class,'mySemesters']);
     Route::apiResource('quizzes',QuizzesController::class);
     Route::apiResource('assignments',AssignmentController::class);
 });
