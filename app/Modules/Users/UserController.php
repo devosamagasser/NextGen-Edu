6af2ac6a5f5bb318f933f8880a4f9df6673d39c8
devosamagasser <devosamagasser@gmail.com>
2025-05-10 03:33:04 +0300

    maintainance v6

--- app/Modules/Users/UserController.php
+++ app/Modules/Users/UserController.php
@@ -16,27 +16,9 @@ class UserController extends Controller
 
     public function profile()
     {
-        return [
-            "id" => 6,
-            "name" => "osama mohamed",
-            "email" => "20812025000001@zu.edu.eg",
-            "type" => "Student",
-            "avatar" => "nextgenedu-database.azurewebsites.net/storage/avatar.png",
-            "nationality" => "National",
-            "uni_code" => "20812025000001",
-            "personal_id" => "3020812131212",
-            "level" => 7,
-            "class" => "فرقة ثالثة",
-            "department" => "الهندسة المعمارية",
-            "group" => 1
-        ];
-        try{
-            $user = request()->user();
-            AuthServices::loadUserRelations($user);
-            return ApiResponse::success(new UserResource($user));
-        }catch (\Exception $e){
-            throw new AuthenticationException;
-        }
+        $user = request()->user();
+        AuthServices::loadUserRelations($user);
+        return ApiResponse::success(new UserResource($user));
     }
 
     public function update(UserUpdateRequest $request)
