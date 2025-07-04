bc664c8c0cbc7500c734157f13c73952831a0f37
devosamagasser <devosamagasser@gmail.com>
2025-07-04 21:51:38 +0300

    groq 4

--- app/Modules/ChatBot/ChatBotController.php
+++ app/Modules/ChatBot/ChatBotController.php
@@ -81,8 +81,8 @@ class ChatBotController extends Controller
         $data = $response->json();
 
         // احصل على رد الـ AI
-        // $reply = $data['choices'][0]['message']['content'] ?? 'عذرًا، حدث خطأ ما.';
-        $reply = $data ;
+        $reply = $data['choices'][0]['message']['content'] ?? 'عذرًا، حدث خطأ ما.';
+        // $reply = $data ;
         
         // if($reply !== 'عذرًا، حدث خطأ ما.')
         //     $reply =  $this->chatBotService->chatResponse($reply, $student);
